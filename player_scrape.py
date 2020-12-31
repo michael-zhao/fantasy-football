60f068c45f12ed72c045f87f66dd295214fc989d
michael-zhao <michael.xin.zhao@gmail.com>
2020-12-30 21:09:00 -0800

    add input, url

--- player_scrape.py
+++ player_scrape.py
@@ -1,3 +1,8 @@
 """Scrapes all the data for a single player given a season.
 """
+import pandas as pd
 
+player = input("Player name: ")
+year = input("Year: ")
+
+url = f'https://www.pro-football-reference.com/search/search.fcgi?search={player}'
