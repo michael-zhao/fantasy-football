b81f677d3784f54c1fa4b1b59724dc386099b691
michael-zhao <michael.xin.zhao@gmail.com>
2020-12-30 21:30:49 -0800

    added method to find player id

--- player_scrape.py
+++ player_scrape.py
@@ -1,8 +1,16 @@
 """Scrapes all the data for a single player given a season.
 """
 import pandas as pd
+import requests
+
+def find_id(player_name):
+    url = f'https://www.pro-football-reference.com/search/search.fcgi?search={player}'
+    redirect_url = requests.get(url).url
+    player_id = redirect_url.split('/')[5].partition('.')[0]
+    return player_id    
 
 player = input("Player name: ")
 year = input("Year: ")
 
-url = f'https://www.pro-football-reference.com/search/search.fcgi?search={player}'
+print(find_id(player))
+
