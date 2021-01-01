3291eae15e0b7213fdab86c73b04da959b37cb8e
michael-zhao <michael.xin.zhao@gmail.com>
2020-12-31 20:10:43 -0800

    added pandas dataframe list & parsed fantasy url

--- player_scrape.py
+++ player_scrape.py
@@ -2,15 +2,26 @@
 """
 import pandas as pd
 import requests
+from datetime import date, datetime
+import webbrowser
 
 def find_id(player_name):
     url = f'https://www.pro-football-reference.com/search/search.fcgi?search={player}'
     redirect_url = requests.get(url).url
     player_id = redirect_url.split('/')[5].partition('.')[0]
-    return player_id    
+    return player_id
 
-player = input("Player name: ")
-year = input("Year: ")
+player = input("Player name (e.g. Russell Wilson): ")
+year = input("Year (default is latest season): ") or datetime.today().year-1
+player_id = find_id(player)
+fantasy_url = f'https://www.pro-football-reference.com/players/{player_id[0]}/{player_id}/fantasy/{year}'
 
-print(find_id(player))
+# for testing:
+# webbrowser.open(fantasy_url)
+
+# needed to run `pip3 install lxml`
+# reads in the table of weeks/fpts
+df = pd.read_html(fantasy_url)[0]
+# print(df)
+
 
